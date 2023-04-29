003215ca673cb36690f0b34fe6cfb72769244090
Akhil P A <akhilpa1085@gamil.com>
2023-04-30 01:55:07 +0530

    redux and react router setup created

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
+import Banner from './homeBanner/Banner'
+
+const Home = () => {
+  return (
+    <>
+        <Header/>
+        <Banner/>
+    </>
+  )
+}
+
+export default Home
